eaf03c60342cf030606a3e9b31e964fccce2f96d
Albert J. Wong <awong.dev@gmail.com>
2016-04-12 14:57:49 -0700

    Factor our app startup code. Fix golden file.
    Soon, it will be useful to attempt to add more endpoints than the
    loopback rest API onto the exposed server. Thus, move the app object out
    of the hca-api-stub directory.
    
    Also, start creating files based on a more standard node.js structure.
    
    Lastly, fix hasan-1.json to remove the wrapper soap operation node. This
    node is autogenerated by the loopback soap connector so having it in the
    datafile just causes an extra nesting which breaks the call.

--- hca-api-stub/voa-rest.js
+++ hca-api-stub/voa-rest.js
@@ -5,101 +5,92 @@ var fs = require('fs');
 
 // read canned JSON file for loopback's SOAP connector to use.
 var anon1 = JSON.parse(fs.readFileSync(path.join(__dirname, './hasan-1.json'), 'utf8'));
- 
-var app = module.exports = loopback();
 
-app.set('restApiRoot', '/api');
+function attach(app) {
+  app.set('restApiRoot', '/api');
 
-var ds = loopback.createDataSource('soap',
-  {
-    connector: require('loopback-connector-soap'),
-    remotingEnabled: true,
-    // wsdl: 'http://vaausesrapp803.aac.va.gov:7401/voa/voaSvc?wsdl' // The url to WSDL
-    wsdl: path.join(__dirname, './voa.wsdl')
-  });
-
-ds.once('connected', function () {
-
-  // Create the model
-  var VoaService = ds.createModel('VoaService', {});
-
-  // Add the methods
-  VoaService.submit = function (form, cb) {
-
-    console.log('*** intercept');
-    
-    var formPrime = anon1;
-
-    VoaService.saveSubmitForm({saveSubmitFormResult: formPrime}, function (err, response) {
-      console.log('saveSubmitForm: %j', response);
-      var result = response;
-      cb(err, result);
+  var ds = loopback.createDataSource('soap',
+    {
+      connector: require('loopback-connector-soap'),
+      remotingEnabled: true,
+      // wsdl: 'http://vaausesrapp803.aac.va.gov:7401/voa/voaSvc?wsdl' // The url to WSDL
+      wsdl: path.join(__dirname, './voa.wsdl')
     });
-  };
 
-  VoaService.status = function (request, cb) {
-    VoaService.getFormSubmissionStatus({retrieveFormSubmissionStatusRequest: request}, function (err, response) {
-      console.log('getFormSubmissionStatus: %j', response);
-      var result = response;
-      cb(err, result);
-    });
-  };
+  ds.once('connected', function () {
 
-  // Map to REST/HTTP
-  loopback.remoteMethod(
-    VoaService.submit, {
-      accepts: [
-        {arg: 'form', type: 'string', required: true,
-          http: {source: 'query'}}
-      ],
-      returns: {arg: 'result', type: 'string', root: true},
-      http: {verb: 'get', path: '/submit'}
-    }
-  );
+    // Create the model
+    var VoaService = ds.createModel('VoaService', {});
 
-  loopback.remoteMethod(
-    VoaService.status, {
-      accepts: [
-        {arg: 'request', type: 'string', required: true,
-          http: {source: 'query'}}
-      ],
-      returns: {arg: 'result', type: 'string', root: true},
-      http: {verb: 'get', path: '/status'}
-    }
-  );
+    // Add the methods
+    VoaService.submit = function (form, cb) {
 
-  // Expose to REST
-  app.model(VoaService);
+      console.log('*** intercept');
+      
+      var formPrime = anon1;
 
-  // LoopBack REST interface
-  app.use(app.get('restApiRoot'), loopback.rest());
-  
-  // API explorer (if present)
-  try {
-    var explorer = require('loopback-explorer')(app);
-    app.use('/explorer', explorer);
-    app.once('started', function (baseUrl) {
-      console.log('Browse your REST API at %s%s', baseUrl, explorer.route);
-    });
-  } catch (e) {
-    console.log(
-      'Run `npm install loopback-explorer` to enable the LoopBack explorer'
+      VoaService.saveSubmitForm(formPrime, function (err, response) {
+        console.log('saveSubmitForm: %j', response);
+        var result = response;
+        cb(err, result);
+      });
+    };
+
+    VoaService.status = function (request, cb) {
+      VoaService.getFormSubmissionStatus({retrieveFormSubmissionStatusRequest: request}, function (err, response) {
+        console.log('getFormSubmissionStatus: %j', response);
+        var result = response;
+        cb(err, result);
+      });
+    };
+
+    // Map to REST/HTTP
+    loopback.remoteMethod(
+      VoaService.submit, {
+        accepts: [
+          {arg: 'form', type: 'string', required: true,
+            http: {source: 'query'}}
+        ],
+        returns: {arg: 'result', type: 'string', root: true},
+        http: {verb: 'get', path: '/submit'}
+      }
     );
-  }
 
-  app.use(loopback.urlNotFound());
-  app.use(loopback.errorHandler());
+    loopback.remoteMethod(
+      VoaService.status, {
+        accepts: [
+          {arg: 'request', type: 'string', required: true,
+            http: {source: 'query'}}
+        ],
+        returns: {arg: 'result', type: 'string', root: true},
+        http: {verb: 'get', path: '/status'}
+      }
+    );
 
-  if (require.main === module) {
-    app.start();
-  }
+    // Expose to REST
+    app.model(VoaService);
 
-});
+    // LoopBack REST interface
+    app.use(app.get('restApiRoot'), loopback.rest());
+    
+    // API explorer (if present)
+    try {
+      var explorer = require('loopback-component-explorer')(app, { basePath: '/api', mountPath: '/explorer' });
+      app.once('started', function (baseUrl) {
+        console.log('Browse your REST API at %s%s', baseUrl, explorer.route);
+      });
+    } catch (e) {
+      console.log(
+        'Run `npm install loopback-component-explorer` to enable the LoopBack explorer' + e
+      );
+    }
 
-app.start = function () {
-  return app.listen(3000, function () {
-    var baseUrl = 'http://127.0.0.1:3000';
-    app.emit('started', baseUrl);
-    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
+    app.use(loopback.urlNotFound());
+    app.use(loopback.errorHandler());
   });
+}
+
+module.exports = {
+  attach: attach
 };
+ 
--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-require('../hca-api-stub/voa-rest');
+var loopback = require('loopback');
+var voaRest = require('../hca-api-stub/voa-rest');
+
+var app = loopback();
+voaRest.attach(app);
+
+app.listen(3000, function () {
+  var baseUrl = 'http://127.0.0.1:3000';
+  app.emit('started', baseUrl);
+  console.log('LoopBack server listening @ %s%s', baseUrl, '/');
+});
