3ff09696a43beffe5d41005622469239cc8a62ec
Joseph Stewart <joseph.stewart.va@gmail.com>
2016-04-13 10:23:55 -0400

    Clean-up most (except console.log and exports) lint errors.

--- hca-api-stub/voa-rest.js
+++ hca-api-stub/voa-rest.js
@@ -1,15 +1,15 @@
 // liberally copy-pasta'ed from the loopback getFormSubmissionStatus example
-var loopback = require('loopback');
-var path = require('path');
-var fs = require('fs');
+const loopback = require('loopback');
+const path = require('path');
+const fs = require('fs');
 
 // read canned JSON file for loopback's SOAP connector to use.
-var anon1 = JSON.parse(fs.readFileSync(path.join(__dirname, './hasan-1.json'), 'utf8'));
+const anon1 = JSON.parse(fs.readFileSync(path.join(__dirname, './hasan-1.json'), 'utf8'));
 
 function attach(app) {
   app.set('restApiRoot', '/api');
 
-  var ds = loopback.createDataSource('soap',
+  const ds = loopback.createDataSource('soap',
     {
       connector: require('loopback-connector-soap'),
       remotingEnabled: true,
@@ -17,29 +17,27 @@ function attach(app) {
       wsdl: path.join(__dirname, './voa.wsdl')
     });
 
-  ds.once('connected', function () {
-
+  ds.once('connected', () => {
     // Create the model
-    var VoaService = ds.createModel('VoaService', {});
+    const VoaService = ds.createModel('VoaService', {});
 
     // Add the methods
-    VoaService.submit = function (form, cb) {
-
+    VoaService.submit = (form, cb) => {
       console.log('*** intercept');
-      
-      var formPrime = anon1;
 
-      VoaService.saveSubmitForm(formPrime, function (err, response) {
+      const formPrime = anon1;
+
+      VoaService.saveSubmitForm(formPrime, (err, response) => {
         console.log('saveSubmitForm: %j', response);
-        var result = response;
+        const result = response;
         cb(err, result);
       });
     };
 
-    VoaService.status = function (request, cb) {
-      VoaService.getFormSubmissionStatus({retrieveFormSubmissionStatusRequest: request}, function (err, response) {
+    VoaService.status = (request, cb) => {
+      VoaService.getFormSubmissionStatus({ retrieveFormSubmissionStatusRequest: request }, (err, response) => {
         console.log('getFormSubmissionStatus: %j', response);
-        var result = response;
+        const result = response;
         cb(err, result);
       });
     };
@@ -48,22 +46,22 @@ function attach(app) {
     loopback.remoteMethod(
       VoaService.submit, {
         accepts: [
-          {arg: 'form', type: 'string', required: true,
-            http: {source: 'query'}}
+          { arg: 'form', type: 'string', required: true,
+            http: { source: 'query' } }
         ],
-        returns: {arg: 'result', type: 'string', root: true},
-        http: {verb: 'get', path: '/submit'}
+        returns: { arg: 'result', type: 'string', root: true },
+        http: { verb: 'get', path: '/submit' }
       }
     );
 
     loopback.remoteMethod(
       VoaService.status, {
         accepts: [
-          {arg: 'request', type: 'string', required: true,
-            http: {source: 'query'}}
+          { arg: 'request', type: 'string', required: true,
+            http: {source: 'query' } }
         ],
-        returns: {arg: 'result', type: 'string', root: true},
-        http: {verb: 'get', path: '/status'}
+        returns: { arg: 'result', type: 'string', root: true },
+        http: { verb: 'get', path: '/status' }
       }
     );
 
@@ -72,11 +70,11 @@ function attach(app) {
 
     // LoopBack REST interface
     app.use(app.get('restApiRoot'), loopback.rest());
-    
+
     // API explorer (if present)
     try {
-      var explorer = require('loopback-component-explorer')(app, { basePath: '/api', mountPath: '/explorer' });
-      app.once('started', function (baseUrl) {
+      const explorer = require('loopback-component-explorer')(app, { basePath: '/api', mountPath: '/explorer' });
+      app.once('started', (baseUrl) => {
         console.log('Browse your REST API at %s%s', baseUrl, explorer.route);
       });
     } catch (e) {
@@ -93,4 +91,3 @@ function attach(app) {
 module.exports = {
   attach: attach
 };
- 
--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-var loopback = require('loopback');
-var voaRest = require('../hca-api-stub/voa-rest');
+const loopback = require('loopback');
+const voaRest = require('../hca-api-stub/voa-rest');
 
-var app = loopback();
+const app = loopback();
 voaRest.attach(app);
 
-app.listen(3000, function () {
-  var baseUrl = 'http://127.0.0.1:3000';
+app.listen(3000, () => {
+  const baseUrl = 'http://127.0.0.1:3000';
   app.emit('started', baseUrl);
   console.log('LoopBack server listening @ %s%s', baseUrl, '/');
 });
