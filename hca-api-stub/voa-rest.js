ff8740499636d1ff91e43137290b0d32385776e6
Joseph Stewart <joseph.stewart.va@gmail.com>
2016-05-12 12:37:26 -0400

    Add conditional logic to cert/key usage. Add TODOs.

--- hca-api-stub/voa-rest.js
+++ hca-api-stub/voa-rest.js
@@ -1,16 +1,70 @@
+const fs = require('fs');
 const loopback = require('loopback');
 const path = require('path');
+
+// TODO: node.js did not like `securityArtifacts` being declared as `let`. Why?
+var securityArtifacts; // eslint-disable-line
 
 function attach(app) {
   app.set('restApiRoot', '/api');
 
+  // TODO: use `NODE_ENV` should probably determining which endpoint is selected.
   const endpoint = {
+    // ES endpoints for development
     c7401: 'http://vaausesrapp803.aac.va.gov:7401/voa/voaSvc',
     e6401: 'https://vaausesrapp803.aac.va.gov:6401/voa/voaSvc',
     e8432: 'https://vaww.esrdev30.aac.va.gov:8432/voa/voaSvc',
+    // ES endpoints that require keys from PKI team and certs installed by Joshua
     preprod: 'https://vaww.esrpre-prod.aac.va.gov/voa/voaSvc',
     prod: 'https://vaww.esr.aac.va.gov/voa/voaSvc'
   };
+
+  /*
+      TODO: there are alleged certificate chain issues that may cause the following error
+      when posting to the endpoint (this is related to self-signed certificates):
+
+        {
+          "error": {
+            "name": "Error",
+            "status": 500,
+            "message": "unable to verify the first certificate",
+            "code": "UNABLE_TO_VERIFY_LEAF_SIGNATURE",
+            "stack": "Error: unable to verify the first certificate\n    at Error (native)\n    at TLSSocket.<anonymous> (_tls_wrap.js:1003:38)\n    at emitNone (events.js:67:13)\n    at TLSSocket.emit (events.js:166:7)\n    at TLSSocket._finishInit (_tls_wrap.js:570:8)"
+          }
+        }
+
+      The short-term work-around is to set an environment variable that influences the TLS library:
+
+        export NODE_TLS_REJECT_UNAUTHORIZED="0"
+
+      an alternative is to set this in `src/server.js` as:
+
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
+        ...although this feels icky.
+
+     For testing, I've set this in my `lauch.json`'s `env` section.
+  */
+
+  // TODO: these elements should be sourced from a secure store.
+  const cert = path.join(__dirname, './healthcare.application.crt');
+  const key = path.join(__dirname, './healthcare.application.key');
+
+  try {
+    fs.accessSync(cert, fs.R_OK);
+    fs.accessSync(key, fs.R_OK);
+    securityArtifacts = {
+      scheme: 'ClientSSL',
+      certPath: cert,
+      keyPath: key
+    };
+  } catch (ex) {
+    securityArtifacts = null;
+    console.log("Can't read security artifacts. Starting without certificate and key.");
+  }
+
+  // TODO: do we need to consider the possibility that the cert/key disappear between
+  // `try` and `loopback.createDataSource()`?
 
   const ds = loopback.createDataSource('soap',
     {
@@ -18,12 +72,8 @@ function attach(app) {
       remotingEnabled: true,
       wsdl: path.join(__dirname, './voa.wsdl'),
       url: endpoint.preprod,
-      security: {
-        scheme: 'ClientSSL',
-        certPath: path.join(__dirname, './healthcare.application.crt'),
-        keyPath: path.join(__dirname, './healthcare.application.key')
-      },
-      wsdl_options: {
+      security: securityArtifacts,
+      wsdl_options: securityArtifacts === null ? null : { // eslint-disable-line
         rejectUnauthorized: false,
         strictSSL: false,
         requestCert: true
