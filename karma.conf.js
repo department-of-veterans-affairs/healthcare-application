677f121113c7748235c015faa9c83a32e897e6e1
Robbie Holmes <robbiethegeek@gmail.com>
2016-05-16 21:00:18 -0400

    Adding back in the globbing of the client side tests.

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'node_modules/babel-polyfill/dist/polyfill.js',
-      'test/client/*.spec.js?(x)'
+      'test/client/**/*.spec.js?(x)'
     ],
 
 
