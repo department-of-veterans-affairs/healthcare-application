0e7eb6844cff1b0f6dcf5339354a76adf50ce131
Courtney Eimerman-Wallace <crwallace39@gmail.com>
2016-05-16 20:12:41 -0400

    adds submission button text and colors.

--- src/client/components/HealthCareApp.jsx
+++ src/client/components/HealthCareApp.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Scroll from 'react-scroll';
 import _ from 'lodash';
 
+import { connect } from 'react-redux';
 import fetch from 'isomorphic-fetch';
 
 import IntroductionSection from './IntroductionSection.jsx';
@@ -66,13 +67,13 @@ class HealthCareApp extends React.Component {
 
   handleContinue() {
     const path = this.props.location.pathname;
-    const formData = this.context.store.getState().veteran;
-    const sectionFields = this.context.store.getState().uiState.sections[path].fields;
+    const formData = this.props.data;
+    const sectionFields = this.props.uiState.sections[path].fields;
 
-    this.context.store.dispatch(ensureFieldsInitialized(sectionFields));
+    this.props.dispatch(ensureFieldsInitialized(sectionFields));
     if (validations.isValidSection(path, formData)) {
       this.context.router.push(this.getUrl('next'));
-      this.context.store.dispatch(updateCompletedStatus(path));
+      this.props.dispatch(updateCompletedStatus(path));
     }
     this.scrollToTop();
   }
@@ -84,17 +85,18 @@ class HealthCareApp extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const path = this.props.location.pathname;
-    const store = this.context.store;
-    const veteran = store.getState().veteran;
+    // const path = this.props.location.pathname;
+    const veteran = this.props.data;
 
-    // Strip out unnecessary fields that track UI state
-    function reducer(i, d) {
-      return typeof d.value !== 'undefined' ? d.value : d;
+    // Strip out unnecessary fields'
+    function reducer(key, value) {
+      return key === 'dirty' ? undefined : value;
     }
 
-    store.dispatch(updateSubmissionStatus('submitPending'));
-    store.dispatch(updateCompletedStatus(path));
+    // const json = JSON.stringify(veteran, reducer, 4);
+
+    this.props.onUpdateSubmissionStatus('submitPending');
+    // this.props.dispatch(updateCompletedStatus(path));
 
     // POST data to endpoint
     fetch('/api/hca/v1/VoaServices/submit', {
@@ -109,18 +111,25 @@ class HealthCareApp extends React.Component {
       if (!response.ok) {
         throw new Error(response.statusText);
       }
-      store.dispatch(updateSubmissionStatus('submitSucceeded', response.json()));
+      setTimeout(() => {
+        this.props.onUpdateSubmissionStatus('applicationSubmitted', response.json());
+      }, 5000);
     }).catch(error => {
-      store.dispatch(updateSubmissionStatus('submitFailed', error));
+      setTimeout(() => {
+        this.props.onUpdateSubmissionStatus('submitFailed', error);
+      }, 5000);
     });
 
-    this.scrollToTop();
+    // this.scrollToTop();
   }
 
   render() {
     let children = this.props.children;
     let buttons;
-    const path = this.props.location.pathname;
+    let submitButton;
+    let submitButtonText;
+    let submitButtonClass;
+    const submissionStatus = this.props.uiState.submissionStatus;
 
     if (children === null) {
       // This occurs if the root route is hit. Default to IntroductionSection.
@@ -144,25 +153,48 @@ class HealthCareApp extends React.Component {
           afterText="»"/>
     );
 
-    const submitButton = (
-      <ProgressButton
-          onButtonClick={this.handleSubmit}
-          buttonText="Submit Application"
-          buttonClass="usa-button-primary"/>
-    );
+    if (submissionStatus === false) {
+      submitButton = (
+        <ProgressButton
+            onButtonClick={this.handleSubmit}
+            buttonText="Submit Application"
+            buttonClass="usa-button-primary"/>
+      );
+    } else if (submissionStatus === 'submitPending') {
+      submitButton = (
+        <ProgressButton
+            onButtonClick={this.handleSubmit}
+            buttonText="Sending..."
+            buttonClass="usa-button-disabled"/>
+      );
+    } else if (submissionStatus === 'applicationSubmitted') {
+      submitButtonText = 'Application Subbmitted';
+      submitButtonClass = 'usa-button-green';
+    } else {
+      submitButton = (
+        <ProgressButton
+            onButtonClick={this.handleSubmit}
+            buttonText="Send Failed"
+            buttonClass="usa-button-secondary"
+            beforeText="x"/>
+      );
+    }
 
-    if (path === '/review-and-submit') {
+    if (this.props.location.pathname === '/review-and-submit') {
       buttons = (
         <div className="row progress-buttons">
           <div className="small-6 medium-5 columns">
             {backButton}
           </div>
-          <div className="small-6 medium-5 end columns">
+          <div className="small-6 medium-5 columns">
             {submitButton}
+          </div>
+          <div className="small-1 medium-1 end columns">
+            <div className={this.state ? 'spinner' : 'hidden'}>&nbsp;</div>
           </div>
         </div>
       );
-    } else if (path === '/introduction') {
+    } else if (this.props.location.pathname === '/introduction') {
       buttons = (
         <div className="row progress-buttons">
           <div className="small-6 medium-5 columns">
@@ -203,12 +235,12 @@ class HealthCareApp extends React.Component {
         <div className="row">
           <Element name="topScrollElement"/>
           <div className="medium-4 columns show-for-medium-up">
-            <Nav currentUrl={path}/>
+            <Nav currentUrl={this.props.location.pathname}/>
           </div>
           <div className="medium-8 columns">
             <div className="progress-box">
             {/* TODO: Figure out why <form> adds fields to url, and change action to reflect actual action for form submission. */}
-              <div className={path === '/review-and-submit' ? '' : 'form-panel'}>
+              <div className="form-panel">
                 {children}
                 {buttons}
               </div>
@@ -220,11 +252,32 @@ class HealthCareApp extends React.Component {
   }
 }
 
-// TODO(awong): Hack to allow access to the store for now while migrating.
-// All uses of this.context.store in this file are WRONG!!!
-HealthCareApp.contextTypes = {
-  router: React.PropTypes.object.isRequired,
-  store: React.PropTypes.object
+HealthCareApp.propTypes = {
+  currentUrl: React.PropTypes.string.isRequired
 };
 
-export default HealthCareApp;
+HealthCareApp.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    data: state.veteran,
+    uiState: state.uiState,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onStateChange: (field, update) => {
+      // dispatch(veteranUpdateField(field, update));
+    },
+    onUpdateSubmissionStatus: (value) => {
+      dispatch(updateSubmissionStatus(value));
+    }
+  };
+}
+
+// TODO(awong): Remove the pure: false once we start using ImmutableJS.
+export default connect(mapStateToProps, mapDispatchToProps, undefined, { pure: false })(HealthCareApp);
+export { HealthCareApp };
