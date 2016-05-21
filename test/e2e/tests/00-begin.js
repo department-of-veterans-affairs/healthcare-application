e7daf52b17d7ce3eedd0abec4dcab5b9f7c01a26
Alexander Ose <alex@alexose.com>
2016-05-20 23:20:13 -0700

    write actual test snippet

---
+++ test/e2e/tests/00-begin.js
@@ -0,0 +1,21 @@
+const config = require('../../../config');
+
+// FIXME: This should come in from a config variable
+const url = 'http://localhost:' + config.port; // eslint-disable-line
+
+module.exports = {
+  'Begin application': (client) => {
+    console.log(url);
+    client
+      .url(url)
+      .waitForElementVisible('body', 1000)
+      .assert.title('Apply for Health Care: Vets.gov')
+      .waitForElementVisible('.form-panel', 3000)
+      .click('.form-panel .usa-button-primary')
+      .pause(1000)
+      .assert.visible('input[name="fname"]')
+      .setValue('input[name="fname"]', 'William')
+      .setValue('input[name="lname"]', 'Shakespeare');
+      // etc...
+  }
+};
