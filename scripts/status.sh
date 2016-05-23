465c22e99ccd9de0a53151ddbdfa69e16308733b
Albert J. Wong <awong.dev@gmail.com>
2016-05-23 14:51:40 -0700

    Address comments from Robby and Alex.
    Add TODOs, use a singular for the REST endpoint, fix up scripts.

--- scripts/status.sh
+++ scripts/status.sh
@@ -4,5 +4,5 @@
 # Prints backend results upon completion.
 [ "$1" = "" ] && exit
 curl -X GET --header "Accept: application/json" \
-  "http://localhost:3000/api/hca/v1/VoaServices/status?request=%7B%22formSubmissionId%22%3A"$1"%7D"
+  "http://localhost:3000/api/hca/v1/application/$1"
 echo
