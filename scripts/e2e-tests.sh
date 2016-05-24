e6d11fa387a066c1d99b4c0517a3c3ae53960dec
Alexander Ose <alex@alexose.com>
2016-05-24 15:18:08 -0700

    add pwd

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # For running integration tests in travis.  This should happen any time we merge to staging.
 npm run webpack-prod;
+pwd
+echo "Linking generated assets..."
 rm -rf generated/dev;
 ln -sf generated/prod generated/dev; # TODO(alexose): find a better workaround
 npm run serve &
