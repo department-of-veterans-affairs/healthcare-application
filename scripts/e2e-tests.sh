f1832047d0bc15bc4877759a417ce4554200f28e
Alexander Ose <alex@alexose.com>
2016-05-24 14:24:31 -0700

    update scripts

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # For running integration tests in travis.  This should happen any time we merge to staging.
 npm run webpack-prod;
-ln -sf generated/prod generated/dev; # TODO(alexose): find a better workaround 
+ln -sf generated/prod generated/dev; # TODO(alexose): find a better workaround
 npm run serve &
 sleep 3;
 nightwatch --config test/e2e/nightwatch.js --env travis
