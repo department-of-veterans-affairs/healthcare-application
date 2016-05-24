9ddfbec9c7d0773755dd54b63360f65b95b62bfb
Alexander Ose <alex@alexose.com>
2016-05-24 15:47:43 -0700

    remove workaround

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # For running integration tests in travis.  This should happen any time we merge to staging.
 npm run webpack-prod;
-pwd
-echo "Linking generated assets..."
-rm -rf generated/dev;
-ln -sf generated/prod generated/dev; # TODO(alexose): find a better workaround
 npm run serve &
 sleep 3;
 nightwatch --config test/e2e/nightwatch.js --env travis
