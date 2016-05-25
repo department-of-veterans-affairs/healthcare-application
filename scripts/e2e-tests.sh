08c4881909e612cc5f644947e0c2e647204ec1f9
Alexander Ose <alex@alexose.com>
2016-05-25 09:15:53 -0700

    remove sleep statement in favor of something more robust

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # For running integration tests in travis.  This should happen any time we merge to staging.
-npm run webpack-prod;
-npm run serve &
-sleep 3;
+
+# Run webpack-prod and start server
+npm start &
+
+# Wait for server to begin accepting connections
+# via http://unix.stackexchange.com/questions/5277
+while ! echo exit | nc localhost 3000; do sleep 1; done
+
 nightwatch --config test/e2e/nightwatch.js --env travis
