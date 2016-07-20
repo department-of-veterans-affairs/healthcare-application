6abc77088bc07f6d1059161edd578b02930dfaf7
Austin <amartin857+github@gmail.com>
2016-07-20 14:46:09 -0700

    Remove lodash from section

--- src/client/components/household-information/FinancialDisclosureSection.jsx
+++ src/client/components/household-information/FinancialDisclosureSection.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import _ from 'lodash';
 
 import ErrorableCheckbox from '../form-elements/ErrorableCheckbox';
 import { isValidFinancialDisclosure } from '../../utils/validations';
