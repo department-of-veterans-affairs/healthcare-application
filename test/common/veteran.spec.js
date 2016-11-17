d3a8f776debc00b5ba941b6401fd27cc65503611
Lihan Li <frankieteardrop@gmail.com>
2016-11-16 19:29:57 -0500

    use package version of schema

--- test/common/veteran.spec.js
+++ test/common/veteran.spec.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 chai.should();
 
 const veteran = require('../../src/common/veteran');
-const applicationSchema = require('../../src/common/schema/application');
+const applicationSchema = require('vets-json-schema').healthcareApplication;
 const validate = require('../../src/common/schema/validator').compile(applicationSchema);
 const fakeApplication = require('../data/fake-application.json');
 
