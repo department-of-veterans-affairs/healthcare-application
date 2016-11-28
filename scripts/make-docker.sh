8378797f19fcb3fcaa0cd080bdcf8970bb6801be
Kenneth Koski <ken@adhocteam.us>
2016-11-28 15:59:46 -0600

    Removing old scripts and tests

--- scripts/make-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-npm run webpack
-mv npm-shrinkwrap.json npm-shrinkwrap.json.old
-npm shrinkwrap
-docker build -t 'dsva/healthcare-application:latest' .
-mv npm-shrinkwrap.json.old npm-shrinkwrap.json
