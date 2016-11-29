a46fd7e4a6b787f93c18cfabcb5d6e569a5935d1
Kenneth Koski <kennethkoski@gmail.com>
2016-11-29 14:42:08 -0600

    Adding basic Jenkinsfile (#301)
    * Adding basic Jenkinsfile
    
    * Testing minor change
    
    * Whoops, updating syntax
    
    * Using npm-cache
    
    * Installing npm-cache globally
    
    * Moving install of npm-cache to devops
    
    * Adding test command
    
    * Removing npm-cache for now
    
    * Removing old scripts and tests
    
    * Someday, we'll all wake up and wonder how JS became so popular
    
    * Renaming Jenkins slave

--- scripts/release-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-SCRIPT_DIR=$(dirname $0)
-
-${SCRIPT_DIR}/make-docker.sh
-docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-docker push 'dsva/healthcare-application:latest'
